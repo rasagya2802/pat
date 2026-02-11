890ab640a7d42deed565c3313d61bb2107873e21
rasagya2802 <155238688+rasagya2802@users.noreply.github.com>
2026-02-12 02:01:19 +0530

    Add reorderList method to ReorderLL class
    Implemented the reorderList method to reorder a linked list as per LeetCode problem 143.

--- Day1/ReorderLL.java
+++ Day1/ReorderLL.java
@@ -1,3 +1,46 @@
-public class ReorderLL {
-    //leetcode question number 143
+
+//leetcode style
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class ReorderLL {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null&&fast.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode prev = null;        
+        while(slow!=null)
+        {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        ListNode currfirst = head;
+        ListNode currsecond = prev;
+        while(currsecond.next!=null)
+        {
+            ListNode temp1 = currfirst.next;
+            ListNode temp2 = currsecond.next;
+            
+            currfirst.next = currsecond;
+            currsecond.next = temp1;
+
+            currfirst = temp1;
+            currsecond = temp2;
+        }
+    }
 }
