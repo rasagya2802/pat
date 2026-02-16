133db07cc38f43c81c9c57ba4349ef6ad9928b39
rasagya2802 <rasagyagade38@gmail.com>
2026-02-16 09:40:35 +0530

    added first file

---
+++ Day6_P1/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//leetcode 204
+public class Main {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    boolean[] p = new boolean[n + 1];
+    for (int i = 2; i <= n; i++) 
+        p[i] = true;
+    
+    for (int i = 2; i * i <= n; i++) 
+        if (p[i]) 
+            for (int j = i * i; j <= n; j += i) 
+                p[j] = false;
+            
+    for (int i = 2; i <= n; i++) 
+        if (p[i]) System.out.print(i + " ");
+  }
+}
