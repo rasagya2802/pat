b41494a78a65f6b4a1aaa51dd87c6097dfafcf5e
rasagya2802 <rasagyagade38@gmail.com>
2026-02-18 20:32:11 +0530

    added the necessary webs

---
+++ Day8/ShortestPalindrome.java
@@ -0,0 +1,12 @@
+package Day8;
+
+//leetcode 214
+//leetcode 1392
+//leetcode 1358
+//prepinsta - aptitude
+//indiabix - aptitude and technical along with company question papers
+//glassdoor - for interview experiences
+//
+public class ShortestPalindrome {
+    
+}
